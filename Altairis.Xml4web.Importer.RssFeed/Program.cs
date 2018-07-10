f9d1672171909bc231be16b8a043781afd4e55a1
Michal Altair Valášek <michal.valasek@altairis.cz>
2018-07-10 15:26:27 +0200

    Added RSS feed importer project

---
+++ Altairis.Xml4web.Importer.RssFeed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Altairis.XML4web.Importer.RssFeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
